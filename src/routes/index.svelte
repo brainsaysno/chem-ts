a987f7d812a5e4e87499442bc1ef1c5b5fc5908c
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2022-06-29 13:56:51 -0300

    feat: add basic ui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,38 @@
 <script lang="ts">
+	import '../app.css';
+
 	import P5 from 'p5-svelte';
 	import { Hydrogen } from '../lib/Atom';
 
-	const sketch = (p5: any) => {
-		const hydrogen = new Hydrogen();
-		let linked = [];
-		for (let i = 0; i < 3; i++) {
-			linked.push(new Hydrogen());
-		}
-		linked.forEach((l) => hydrogen.link(l));
-		linked[0].link(new Hydrogen(), 2);
-		linked[0].link(new Hydrogen(), 3);
-		linked[0].link(new Hydrogen());
+	const canvasWidth = 500;
+	const canvasHeight = 500;
 
+	const hydrogen = new Hydrogen();
+	let linked = [];
+	for (let i = 0; i < 3; i++) {
+		linked.push(new Hydrogen());
+	}
+	linked.forEach((l) => hydrogen.link(l));
+	linked[0].link(new Hydrogen(), 2);
+	linked[0].link(new Hydrogen(), 3);
+	linked[0].link(new Hydrogen());
+
+	const sketch = (p5: any) => {
 		p5.setup = () => {
-			p5.createCanvas(400, 400);
+			p5.createCanvas(canvasWidth, canvasHeight);
 			hydrogen.draw(p5);
 		};
-
-		/* 		p5.draw = () => {
-			p5.clear();
-		}; */
 	};
 </script>
 
-<P5 {sketch} parentDivStyle="background-color:white" />
+<main class="h-screen w-screen flex justify-center items-center flex-col gap-5">
+	<div class="text-center">
+		<h1 class="text-3xl font-bold">Chem TS</h1>
+		<h2 class="text-md font-medium">
+			A chemistry molecule visualization tool made with svelte, p5 and Typescript.
+		</h2>
+	</div>
+	<div class="w-100 h-100 bg-gray-200 border-2 border-black">
+		<P5 {sketch} />
+	</div>
+</main>
