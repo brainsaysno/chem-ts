1acccd1490f6198b908f1b9c8d5f6427a7970c7a
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2022-06-29 14:11:56 -0300

    fix: change titles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,9 +27,14 @@
 
 <main class="h-screen w-screen flex justify-center items-center flex-col gap-5">
 	<div class="text-center">
-		<h1 class="text-3xl font-bold">Chem TS</h1>
+		<h1 class="text-3xl font-bold">👨‍🔬 Chem TS 👩‍🔬</h1>
 		<h2 class="text-md font-medium">
-			A chemistry molecule visualization tool made with svelte, p5 and Typescript.
+			A chemistry molecule visualization tool made with <a
+				href="https://svelte.dev/"
+				class="text-orange-500 font-semibold">Svelte</a
+			>,
+			<a href="https://p5js.org/" class="text-red-500 font-semibold">p5</a> and
+			<a href="https://www.typescriptlang.org/" class="text-blue-500 font-semibold">TypeScript</a>.
 		</h2>
 	</div>
 	<div class="w-100 h-100 bg-gray-200 border-2 border-black">
